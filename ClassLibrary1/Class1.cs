b8a3eb11d60964077cd67d4514fb2e66472fb8f2
Юлия Селукова <jullia_63@mail.ru>
2017-12-06 21:19:26 +0600

    первая фиксация

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public ulong Factorial(ulong n)
+        {
+            ulong F = 1;
+            for (ulong i = 1; i <= n; i++)
+                F *= i;
+            return F;
+        }
+    }
+}
+           
---
+++ WindowsFormsApp-Factorial/Form1.cs
@@ -0,0 +1,38 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp_Factorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ulong n = 0;
+            if (ulong.TryParse(textBox1.Text, out n)&&(n>=1))
+            {
+                Class1 obj = new Class1();
+                ulong F = obj.Factorial(n);
+                label1.Text = "факториал = "+F;
+            }
+            else
+            {
+                label1.Text = "Некорректно введено число"; 
+            }
+
+        }
+    }
+}
